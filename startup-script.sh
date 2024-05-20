cbe029168387a8ed83837628569959fea9230d39
JavierP08 <88795402+JavierP08@users.noreply.github.com>
2024-05-20 06:35:17 +0000

    startup-script update

--- startup-script.sh
+++ startup-script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+docker pull cassandra:latest
 docker network create cassandra
 docker run --rm -d --name cassandra --hostname cassandra --network cassandra cassandra
-sleep 20  # Espera a que Cassandra esté completamente levantada
-docker run --rm --network cassandra -v "/workspace/data.cql:/scripts/data.cql" -e CQLSH_HOST=cassandra -e CQLSH_PORT=9042 -e CQL_VERSION=3.4.6 cassandra cqlsh -f /scripts/data.cql
+docker run --rm --network cassandra -v "$(pwd)/data.cql:/scripts/data.cql" -e CQLSH_HOST=cassandra -e CQLSH_PORT=9042 -e CQLVERSION=3.4.6 nuvo/docker-cqlsh
+docker run --rm -it --network cassandra nuvo/docker-cqlsh cqlsh cassandra 9042 --cqlversion='3.4.6'

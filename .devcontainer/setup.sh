6b7699d4ca1f9fcc738845c387a3eb620b639988
JavierP08 <88795402+JavierP08@users.noreply.github.com>
2024-05-20 05:59:01 +0000

    Primer commit

--- .devcontainer/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Esperar a que Cassandra esté lista para aceptar conexiones
-while ! cqlsh -e "describe keyspaces"; do
-  sleep 1
-done
-
-# Crear esquemas o cargar datos
-cqlsh -f "/workspace/your-repo-path/schema.cql"
-cqlsh -f "/workspace/your-repo-path/data.cql"
